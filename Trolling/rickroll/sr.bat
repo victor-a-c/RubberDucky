6ecab9a1de5a27c25ae7cadd558bff132e2bb513
TheV <37463626+victor-a-c@users.noreply.github.com>
2022-04-29 01:22:05 -0300

    batch file added
    original batch file added for testing prior to deployment

---
+++ Trolling/rickroll/sr.bat
@@ -0,0 +1,5 @@
+if not DEFINED IS_MINIMIZED set IS_MINIMIZED=1 && start "" /min "%~dpnx0" %* && exit
+TIMEOUT /T 600
+START "" https://www.youtube.com/watch?v=xm3YgoEiEDc
+START Powershell -command "$wshell = New-Object -ComObject wscript.shell;[system.Diagnostics.Process]::Start(\"msedge\",\"about:blank\");Sleep 1;$wshell.SendKeys('^"{l}"');Sleep 1;$wshell.SendKeys('https://fakeupdate.net/win10ue/');$wshell.SendKeys('"{Enter}"');$wshell.SendKeys('"{F11}"')"
+exit
